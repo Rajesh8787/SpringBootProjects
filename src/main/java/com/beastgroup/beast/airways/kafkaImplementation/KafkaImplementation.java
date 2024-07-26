303a45b87de11745239b8b276b05b6a116a67d9b
unknown <Beast@DESKTOP-FMNP9KP>
2024-07-26 13:01:37 +0530

    added Kafka here

---
+++ src/main/java/com/beastgroup/beast/airways/kafkaImplementation/KafkaImplementation.java
@@ -0,0 +1,7 @@
+package com.beastgroup.beast.airways.kafkaImplementation;
+
+public class KafkaImplementation {
+    public static void main(String[] args){
+        System.out.println("Hey mate! I  am Kafka there, How are you");
+    }
+}
