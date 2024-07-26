ce833e77a72d9496ba14e452e105daf66911d14f
unknown <Beast@DESKTOP-FMNP9KP>
2024-07-26 13:33:48 +0530

    creatng git name brancht

--- src/main/java/com/beastgroup/beast/airways/responce/AirwaysResponce.java
+++ src/main/java/com/beastgroup/beast/airways/responce/AirwaysResponce.java
@@ -3,16 +3,5 @@ package com.beastgroup.beast.airways.responce;
 import org.springframework.scheduling.support.SimpleTriggerContext;
 
 public class AirwaysResponce {
-    private String msg;
-    //Constructor
-    public AirwaysResponce(String msg){
-        this.msg = msg;
-    }
-    //Getter&Setter
-    public String getMsg(){
-        return msg;
-    }
-    public void setMsg(String msg){
-        this.msg = msg;
-    }
+
 }
